ad18ecd2916db2d55f96bc8bcbf55776d80cf167
CamiloSalvo <camilo.salvo@usach.cl>
2016-11-06 19:49:37 -0300

    Implementa diseño

--- js/controllers/festivales/festivalesViewCtrl.js
+++ js/controllers/festivales/festivalesViewCtrl.js
@@ -1,13 +1,11 @@
 app.controller("FestivalesViewCtrl", function($scope, $routeParams, FestivalService){
     var vm = this;
     vm.festival = {};
-    console.log($routeParams.id);
 
     getFestival();
     function getFestival() {
         FestivalService.getResource($routeParams.id).then(function(data) {
             vm.festival = data.data;
-            console.log('data', data);
         });
     }
 });
