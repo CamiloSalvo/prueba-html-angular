0e5a7e6b03874f225d556a821ab71e332be5b523
Matias Vargas <matiasvm@gmail.com>
2016-12-11 19:33:57 -0300

    Modifica infografía para mostrar dos tipos de graficos

--- js/controllers/infografias/infografiaListCtrl.js
+++ js/controllers/infografias/infografiaListCtrl.js
@@ -4,11 +4,13 @@ app.controller("InfografiaListCtrl", function($scope, TweetService){
     vm.dataLoaded = false;
     vm.fechaInicio = '';
     vm.fechaTermino = '';
+    vm.selectTipo = 'Cantidad'
 
     TweetService.getFechas('2016-10-10', '2016-10-20')
     .then(function(data) {
       vm.datos = data;
       vm.dataLoaded = true;
+      console.log('data', data);
     })
 
     vm.options = {
@@ -18,6 +20,39 @@ app.controller("InfografiaListCtrl", function($scope, TweetService){
         width: 1120, // HAY QUE AJUSTAR ESTO A CADA PANTALLA QUE USEMOS.
         x: function(d){return d3.time.format("%Y-%m-%d").parse(d.fecha);},
         y: function(d){return d.contador;},
+        margin: {
+          top: 0,
+          right: 35,
+          bottom: 70,
+          left: 70
+        },
+        useInteractiveGuideline: true,
+        dispatch: {},
+        xAxis: {
+          tickFormat: (function(d) {
+          return d3.time.format('%Y-%m-%d')(d)
+          }),
+          ticks: 5,
+          axisLabel: "Fechas (Día)"
+        },
+        xScale: d3.time.scale(),
+        yAxis: {
+          axisLabel: "Cantidad (#)"
+        }
+      },
+      title: {
+        enable: true,
+        text: "Gráfico de Festivales"
+      }
+    };
+
+    vm.options2 = {
+      chart: {
+        type: "lineChart",
+        height: 500,
+        width: 1120, // HAY QUE AJUSTAR ESTO A CADA PANTALLA QUE USEMOS.
+        x: function(d){return d3.time.format("%Y-%m-%d").parse(d.fecha);},
+        y: function(d){return d.score;},
         margin: {
           top: 0,
           right: 35,
